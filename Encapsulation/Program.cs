058c19efcc9764b64505104ece928ff31bce2e75
Margarita <Margarita@DATA_SERVER>
2021-11-12 16:33:39 +0400

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Properties 
+            Bank bank = new Bank();
+            bank.Balance = 100;
+            Console.WriteLine(bank.Balance);
+
+            bank.Balance = -50;
+            //Console.WriteLine(bank.Balance);
+
+            Console.ReadKey();
+        }
+    }
+
+    //Properties example
+    public class Bank
+    {
+        private double _balance;
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                // validate the value
+                if (value < 0)
+                {
+                    Console.WriteLine("value cannot be negative");
+                }
+                else
+                {
+                    _balance = value;
+                }
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton 
+            Singleton ob = Singleton.Instance;
+            Singleton.Show(ob);
+
+            Singleton ob2 = Singleton.Instance;
+            Singleton.Show(ob2);
+
+            Singleton ob3 = Singleton.Instance;
+            Singleton.Show(ob3);
+
+            Console.ReadKey();
+        }
+    }
+
+    //Singleton example
+    class Singleton
+    {
+        private static int counter = default(int);
+        private static Singleton _instance;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Singleton();
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"The constructor called {counter} time");
+        }
+
+        public static void Show(Singleton instance)
+        {
+            Console.WriteLine(instance.GetHashCode());
+        }
+    }
+}
